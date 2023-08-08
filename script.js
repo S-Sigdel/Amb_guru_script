64efc6e10b2dbd656b881927fe6230dcbc88b5a7
Momo <sakshyamsigdel@gmail.com>
2023-08-08 16:08:40 +0545

    Reupdate for the space bar validation

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 document.addEventListener('keydown', function (event) {
   const videoPlayer = document.querySelector('video'); // Replace 'video' with the actual selector for your video element
 
-  // Skip 10 seconds forward or backward
-  if (videoPlayer && (event.key === 'ArrowRight' || event.key === 'ArrowLeft')) {
-    const skipAmount = (event.key === 'ArrowRight') ? 10 : -10;
-    videoPlayer.currentTime += skipAmount;
-  } else if (event.key === ' ') {
-    event.preventDefault(); // Prevent default space bar functionality
-    clickButton(); // Call the function to click the button
+  if (videoPlayer) {
+    if (event.key === 'ArrowRight' || event.key === 'ArrowLeft') {
+      // Skip 10 seconds forward or backward
+      const skipAmount = (event.key === 'ArrowRight') ? 10 : -10;
+      videoPlayer.currentTime += skipAmount;
+    } else if (event.key === ' ') {
+      event.preventDefault(); // Prevent default space bar functionality
+      togglePlayPause(videoPlayer); // Call the function to toggle play/pause
+    }
   }
 });
 
-// Function to click the play button
-function clickButton() {
-  const playButton = document.querySelector('.vjs-play-control'); // Select the button by its class
-  if (playButton) {
-    playButton.click(); // Simulate a click on the play button
+// Function to toggle play/pause of the video
+function togglePlayPause(videoPlayer) {
+  if (videoPlayer.paused || videoPlayer.ended) {
+    videoPlayer.play(); // Play the video
+  } else {
+    videoPlayer.pause(); // Pause the video
   }
 }
