ba3011a2fe271e2daaaf0dd326f83e920603f374
Momo <sakshyamsigdel@gmail.com>
2023-08-08 15:56:28 +0545

    Update, combined event listeners

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 document.addEventListener('keydown', function (event) {
   const videoPlayer = document.querySelector('video'); // Replace 'video' with the actual selector for your video element
 
-  if (event.key === 'ArrowRight') {
-    // Skip 10 seconds forward
-    if (videoPlayer) {
-      videoPlayer.currentTime += 10;
-    }
-  } else if (event.key === 'ArrowLeft') {
-    // Skip 10 seconds backward
-    if (videoPlayer) {
-      videoPlayer.currentTime -= 10;
-    }
+  // Skip 10 seconds forward or backward
+  if (videoPlayer && (event.key === 'ArrowRight' || event.key === 'ArrowLeft')) {
+    const skipAmount = (event.key === 'ArrowRight') ? 10 : -10;
+    videoPlayer.currentTime += skipAmount;
+  } else if (event.key === ' ') {
+    event.preventDefault(); // Prevent default space bar functionality
+    clickButton(); // Call the function to click the button
   }
 });
 
-
-  // Function to click the button
-  function clickButton() {
-    const button = document.querySelector('.vjs-play-control'); // Select the button by its class
-    if (button) {
-      button.click(); // Simulate a click on the button
-    }
+// Function to click the play button
+function clickButton() {
+  const playButton = document.querySelector('.vjs-play-control'); // Select the button by its class
+  if (playButton) {
+    playButton.click(); // Simulate a click on the play button
   }
-
-  // Event listener for the space bar key press
-  document.addEventListener('keydown', function (event) {
-    if (event.key === ' ') { // ' ' represents the space bar key
-      event.preventDefault(); // Prevent default space bar functionality
-      clickButton(); // Call the function to click the button
-    }
-  });
+}
